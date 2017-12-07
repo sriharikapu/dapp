86ee512a5c18b51f809e96b2bff81167d29b6451
richmcateer <rmcateer1@gmail.com>
2017-12-07 15:39:56 -0500

    Remove kiosk.js

--- src/kiosk.js/contracts/contracts/Resolver.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.11;
-
-/** @title Base resolver contract */
-contract Resolver {
-    function supportsInterface(bytes4 interfaceID) public constant returns (bool);
-}
--- src/kiosk.js/contracts/contracts/URLResolver.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.11;
-
-import "./Resolver.sol";
-
-/** @title Resolver contract that specifies an API endpoint where product information can be retrieved */
-contract URLResolver is Resolver {
-    function productURL(uint256 DIN) public constant returns (string);
-}
